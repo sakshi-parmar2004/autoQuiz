fa1ec6fc2b9cb83d95b41187782542fdb84267e4
Sakshi Parmar <140688243+sakshi-parmar2004@users.noreply.github.com>
2025-04-23 08:35:57 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,8 +17,8 @@ const PORT= process.env.PORT || 3000;
 app.use(express.urlencoded({extended:false}))
 app.use(cors({
   origin: 'https://autoquiz-frontend.onrender.com',
-  // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  // allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 
